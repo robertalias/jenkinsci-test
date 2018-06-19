c9419afcf8138b2d64e4dbe95181c59d3053c96a
justbeay <robert.li@supernovacompanies.biz>
2018-06-19 10:44:23 +0800

    update

--- script/Jenkinsfile.groovy
+++ script/Jenkinsfile.groovy
@@ -1,6 +1,7 @@
 #!/usr/bin/env groovy
 
 node {
+    update_commit_status('justbeay', 'jenkinsci-test', params.PULL_REQUEST_NUMBER, 'pending')
     echo "======== TRIGGER_BRANCH:${params.TRIGGER_BRANCH}, TRIGGER_Boolean:${params.TRIGGER_Boolean}, TRIGGER_Choice:${params.TRIGGER_Choice} ======="
     stage 'compile'
     dir('src'){
@@ -18,4 +19,20 @@ node {
     }
     // stage 'deploy'
     echo "======== finish ${env.JOB_NAME}, with build number:${env.BUILD_NUMBER} ========"
+    update_commit_status('justbeay', 'jenkinsci-test', params.PULL_REQUEST_NUMBER, 'success')
+}
+
+def update_commit_status(owner, repository, pullNumber, state) {
+    if(pullNumber){
+        withCredentials([usernamePassword(credentialsId: 'GITHUB_ACCESS_TOKEN', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]){
+            sh "./jenkins-ci/git_commit_status.py --user ${USERNAME} --token ${PASSWORD}" +
+                " --owner ${owner}" +
+                " --repository ${repository}" +
+                " --pull-number ${pullNumber}" +
+                " --state ${state}" +
+                // " --target-url '${BUILD_URL}'"
+                " --description 'jenkins build job:${BUILD_NUMBER} ${state}'"
+                " --context 'continuous-integration/jenkins'"
+        }
+    }
 }
--- script/initParam.groovy
+++ script/initParam.groovy
@@ -4,7 +4,9 @@ properties([
     parameters([
         string(name: 'TRIGGER_BRANCH', defaultValue: 'dev', description: 'the git branch name', trim: false),
         booleanParam(name: 'TRIGGER_Boolean', defaultValue: true, description: 'service triigger'),
-        choice(name: 'TRIGGER_Choice', choices: 'aaa\nbbb\nccc\nxxx\nyyy', defaultValue: 'xxx', description: '')
+        choice(name: 'TRIGGER_Choice', choices: 'aaa\nbbb\nccc\nxxx\nyyy', defaultValue: 'xxx', description: ''),
+        string(name: 'NOTICE_EMAIL', defaultValue: '', description: '	build result log email receiver, split by \';\'', trim: false),
+        string(name: 'PULL_REQUEST_NUMBER', defaultValue: '', description: 'github pull_request number, used to update pull commits status', trim: false)
     ]),
     pipelineTriggers([
         parameterizedCron('H(0-5) 0 * * 1 % TRIGGER_Boolean=true')
